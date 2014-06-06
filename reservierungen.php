98c7d2e337f805e71d57ff5920c416226124c8e7
Lukas Winkler <l.winkler23@me.com>
2014-06-06 14:28:39 +0200

    PHP-Code verbessert

--- reservierungen.php
+++ reservierungen.php
@@ -4,6 +4,16 @@
 		<meta charset="utf-8">
 
 		<title>Reservierungen</title>
+		<style type="text/css">
+			.Ja {
+				background-color: #99FF99;
+				display:block;
+			}
+			.Nein {
+				background-color: #FF9999;
+				display:block;
+			}
+		</style>
 	</head>
 
 	<body>
@@ -14,11 +24,16 @@ session_start();
 if (isset($_SESSION['user']) && isset($_SESSION['user_id'])) {
 
 require_once "verbindungsaufbau.php"; //mit Server verbinden
-$ergebnis = $mysqli->query("SELECT reservierungen.reserv_id,vorname,nachname,telefonnummer,email,anzahl,datum,bezahlt,name FROM reservierungen,bearbeitungen,benutzer WHERE reservierungen.reserv_id = bearbeitungen.reserv_id AND bearbeitungen.user_id = benutzer.user_id");
+$ergebnis = $mysqli->query("SELECT reserv_id,vorname,nachname,telefonnummer,email,anzahl,datum,bezahlt,name,bearb_datum FROM reservierungen LEFT JOIN benutzer ON bearbeiter = user_id");
 echo "<table border='1'>\n";
-echo "<tr><th>Vorname</th><th>Nachname</th><th>Telefon</th><th>E-Mail</th><th>Anzahl</th><th>Preis</th><th>Datum</th><th>Bezahlt</th><th>bearbeitet von</th>"; //Zeile mit Überschriften
+echo "<tr><th>Vorname</th><th>Nachname</th><th>Telefon</th><th>E-Mail</th><th>Anzahl</th><th>Preis</th><th>Datum</th><th>Bezahlt</th><th>bearbeitet von</th><th>Datum</th>"; //Zeile mit Überschriften
 while ($zeile = $ergebnis->fetch_array()) { // für jeden Wert in der Datenbank eine Tabellenzeile
 		$preis=$zeile["anzahl"]*5;
+		if ($zeile["bezahlt"] == "1") {
+			$bezahlt="Ja";
+		} else {
+			$bezahlt="Nein";
+		}
 		echo "<tr>\n<td>" . htmlspecialchars($zeile["vorname"]) . "</td>\n"
         . "<td>" . htmlspecialchars($zeile['nachname']) . "</td>\n"
         . "<td>" . htmlspecialchars($zeile['telefonnummer']) . "</td>\n"
@@ -26,28 +41,11 @@ while ($zeile = $ergebnis->fetch_array()) { // für jeden Wert in der Datenbank 
         . "<td>" . htmlspecialchars($zeile['anzahl']) . "</td>\n"
         . "<td>" . $preis . "€</td>\n"
         . "<td>" . htmlspecialchars($zeile['datum']) . "</td>\n"
-        . "<td>" . htmlspecialchars($zeile['bezahlt']) . "</td>\n"
-        . "<td>" . htmlspecialchars($zeile['name']) . "</td></tr>\n\n";
+        . "<td class='" . $bezahlt . "'><a href='./bezahlen.php?id=" . htmlspecialchars($zeile['reserv_id']) . "&bezahlt=" . $bezahlt . "'><b>" . $bezahlt . "</b></a></td>\n"
+        . "<td>" . htmlspecialchars($zeile['name']) . "</td>\n"
+        . "<td>" . htmlspecialchars($zeile['bearb_datum']) . "</td>\n</tr>\n\n";
 }
-echo "</table>";
 $ergebnis->close();
-echo "<h2>Noch nicht bezahlt</h2>";
-$ergebnis = $mysqli->query("SELECT reservierungen.reserv_id,vorname,nachname,telefonnummer,email,anzahl,datum FROM reservierungen,bearbeitungen WHERE reservierungen.reserv_id != bearbeitungen.reserv_id");
-echo "<table border='1'>\n";
-echo "<tr><th>Vorname</th><th>Nachname</th><th>Telefon</th><th>E-Mail</th><th>Anzahl</th><th>Preis</th><th>Datum</th>"; //Zeile mit Überschriften
-while ($zeile = $ergebnis->fetch_array()) { // für jeden Wert in der Datenbank eine Tabellenzeile
-		$preis=$zeile["anzahl"]*5;
-		echo "<tr>\n<td>" . htmlspecialchars($zeile["vorname"]) . "</td>\n"
-        . "<td>" . htmlspecialchars($zeile['nachname']) . "</td>\n"
-        . "<td>" . htmlspecialchars($zeile['telefonnummer']) . "</td>\n"
-        . "<td><a href='mailto:" . htmlspecialchars($zeile['email']) . "'>" . htmlspecialchars($zeile['email'])  . "</a></td>\n"
-        . "<td>" . htmlspecialchars($zeile['anzahl']) . "</td>\n"
-        . "<td>" . $preis . "€</td>\n"
-        . "<td>" . htmlspecialchars($zeile['datum']) . "</td>\n"
-		. "<td><a class='tabelle' href='./bezahlen.php?id=" . htmlspecialchars($zeile['reserv_id']) . "&bezahlt=yes'><b>teilnehmen</b></a></td></tr>\n\n";
-		
-}
-echo "</table>";
 $mysqli->close();
 
 
